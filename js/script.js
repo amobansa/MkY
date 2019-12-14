d8052658f0af11c149ad58307fbe95d46ca34906
amobansa <obansaabdulmalik@gmail.com>
2019-12-13 12:47:06 -1200

    Added All files
    Html,Css,JS

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+//Slideshow script
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 9000); // Change image every 2 seconds
+}
+//side bar nav
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
